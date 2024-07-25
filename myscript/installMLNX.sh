94ae1097df6e8145dd9c8f345fc3d11268d56dcf
dyx <782618517@qq.com>
2024-07-25 09:05:33 +0000

    debug

--- myscript/installMLNX.sh
+++ myscript/installMLNX.sh
@@ -5,9 +5,9 @@ if [ ! -d "tmp" ]; then
 fi
 
 cd tmp
-wget https://content.mellanox.com/ofed/MLNX_OFED-23.10-3.2.2.0/MLNX_OFED_LINUX-23.10-3.2.2.0-ubuntu20.04-x86_64.tgz
-tar -xvf MLNX_OFED_LINUX-23.10-3.2.2.0-ubuntu20.04-x86_64.tgz
-cd MLNX_OFED_LINUX-23.10-3.2.2.0-ubuntu20.04-x86_64
+wget https://content.mellanox.com/ofed/MLNX_OFED-4.9-7.1.0.0/MLNX_OFED_LINUX-4.9-7.1.0.0-ubuntu20.04-x86_64.tgz
+tar -xvf MLNX_OFED_LINUX-4.9-7.1.0.0-ubuntu20.04-x86_64.tgz
+cd MLNX_OFED_LINUX-4.9-7.1.0.0-ubuntu20.04-x86_64
 
 sudo ./mlnxofedinstall  --force
 sudo /etc/init.d/openibd restart
